831bf6302ba43cc2a7aad2b2e7847f49df711722
SlesviaLu <2102470H@student.tp.edu.sg>
2022-12-20 14:48:15 +0800

    Created before and after test selenium
    Added src/test/java and added selenium to the pom.xml file, basic
    configurations for before test and after test have been added.
    
    Signed-off-by: SlesviaLu <2102470H@student.tp.edu.sg>

---
+++ src/test/java/com/dvops/maven/eclipse/DVOPSTest.java
@@ -0,0 +1,34 @@
+package com.dvops.maven.eclipse;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class DVOPSTest {
+	  //declare Selenium WebDriver
+	  private WebDriver webDriver;	
+  @Test
+  public void f() {
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  //Setting system properties of ChromeDriver
+	  //to amend directory path base on your local file path
+	  String chromeDriverDir = "C:\\Program Files\\Google\\Chrome\\chromedriver.exe";
+
+	  System.setProperty("webdriver.chrome.driver", chromeDriverDir);
+
+	  //initialize FirefoxDriver at the start of test
+	  webDriver = new ChromeDriver();  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  //Quit the ChromeDriver and close all associated window at the end of test
+	  webDriver.quit();	
+  }
+
+}
